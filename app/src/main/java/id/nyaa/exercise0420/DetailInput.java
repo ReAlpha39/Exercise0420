bc65d9d21a793a3f1329a9f677893decd89b6527
YunusNS <yunusw718@gmail.com>
2020-04-20 11:30:18 +0800

    add layout detail kontak

---
+++ app/src/main/java/id/nyaa/exercise0420/DetailInput.java
@@ -0,0 +1,14 @@
+package id.nyaa.exercise0420;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_input);
+    }
+}
