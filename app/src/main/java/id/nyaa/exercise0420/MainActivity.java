31a9f42bc65802c925d6a94f1009ceaaf60b5b0c
YunusNS <yunusw718@gmail.com>
2020-04-20 18:33:50 +0800

    add item click and item long click action

--- app/src/main/java/id/nyaa/exercise0420/MainActivity.java
+++ app/src/main/java/id/nyaa/exercise0420/MainActivity.java
@@ -1,12 +1,16 @@
 package id.nyaa.exercise0420;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,7 +19,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "MESSAGE";
     private ListView obj;
-    Cursor cursor;
     DBHelper mydb;
 
     @Override
@@ -28,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Contact> contactArrayList = mydb.getAllData();
         ListAdapter listAdapter = new ListAdapter(contactArrayList, this);
         obj.setAdapter(listAdapter);
+        obj.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+                Intent intent = new Intent(getApplicationContext(), DetailInput.class);
+                //intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+        obj.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, obj);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.show();
+                return true;
+            }
+        });
 
         FloatingActionButton floatingActionButton = findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
